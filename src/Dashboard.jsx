adfc7015391ba2620f85164465e6ecc14b7ea591
krish <karora07@student.ubc.ca>
2024-06-23 07:10:36 -0700

    survey

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -10,19 +10,10 @@ const Dashboard = () => {
                 <li className="feature"><Link to="/breathe" className="breathing">
                     <img className="breathing-img" src="https://media-cdn.wimhofmethod.com/uploads/cms/2023/03/breathing-exercises.png"></img>
                     </Link></li>
-                <li><Link to="/breathe" className="breathing">
-                    <img className="breathing-img" src="https://media-cdn.wimhofmethod.com/uploads/cms/2023/03/breathing-exercises.png"></img>
-                    </Link></li>
                 <li className="feature"><Link to="/planner" className="planner">
-                    <img className="planner-img" src="https://i.imgur.com/lwVKoO5.png"></img>
-                    </Link></li>
-                <li><Link to="/planner" className="planner">
                     <img className="planner-img" src="https://cdn0.iconfinder.com/data/icons/small-n-flat/24/678116-calendar-512.png"></img>
                     </Link></li>
                 <li className="feature"><Link to="/tracker" className="tracker">
-                    <img className="tracker-img" src="https://i.imgur.com/Js4HPeK.png"></img>
-                    </Link></li>
-                <li><Link to="/tracker" className="tracker">
                     <img className="tracker-img" src="https://cdn1.iconfinder.com/data/icons/ui-basic-8/64/x-33-512.png"></img>
                     </Link></li>
                 <li className="feature"><Link to="/reminder" className="reminder">
